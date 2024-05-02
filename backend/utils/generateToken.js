8f6f11b5d835407e0921e8b170252d6246e4deb8
phungb2005688 <122144847+phungb2005688@users.noreply.github.com>
2024-05-02 16:46:16 +0700

    update

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -22,8 +22,8 @@ const generateAdminToken = (res, userId) => {
   // Set JWT as an HTTP-Only cookie
   res.cookie('adminJwt', adminToken, {
     httpOnly: true,
-    secure: process.env.NODE_ENV !== 'development',
-    sameSite: 'strict',
+    secure: true,
+    sameSite: 'None',
     maxAge: 30 * 24 * 60 * 60 * 1000,
   });
 };
