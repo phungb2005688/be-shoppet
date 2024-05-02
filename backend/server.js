a05bd0c855ade2ae7e6562017cdcb648fdc76d4e
phungb2005688 <122144847+phungb2005688@users.noreply.github.com>
2024-05-02 15:15:38 +0700

    connect frontend

--- backend/server.js
+++ backend/server.js
@@ -30,6 +30,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
+app.use(cors({
+  origin: 'https://fe-shoppet.vercel.app'  
+}));
+
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/orders', orderRoutes);
@@ -64,10 +68,7 @@ if (process.env.NODE_ENV === 'production') {
 
 app.use(notFound);
 app.use(errorHandler);
-// app.use(cors());
-app.use(cors({
-    origin: 'https://fe-shoppet.vercel.app'  // Thay thế bằng domain của frontend
-}));
+
 app.listen(port, () =>
   console.log(`Server running on port ${port}`)
 );
